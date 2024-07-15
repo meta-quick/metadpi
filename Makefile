bddde2594130567770b66d3f79c84728a00ef0c0
gao.brian <gao.brian@gmail.com>
2024-07-15 13:42:20 +0800

    Rename executeable to mdpi

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 mdpi:
 	CGO_ENABLED=1 go build -o mdpi main.go
 
+clean:
+	rm -rf mdpi
--- core/cmd.go
+++ core/cmd.go
@@ -1,11 +1,11 @@
 package core
 
 import (
-	"os"
-	"strings"
 	"fmt"
 	"net"
+	"os"
 	"strconv"
+	"strings"
 )
 
 const InternalCmdPrefix = "--"
@@ -18,18 +18,18 @@ const (
 )
 
 type Cmd struct {
-	Device string
+	Device     string
 	plugHandle *Plug
 }
 
 func NewCmd(p *Plug) *Cmd {
 
 	return &Cmd{
-		plugHandle:p,
+		plugHandle: p,
 	}
 }
 
-//start
+// start
 func (cm *Cmd) Run() {
 
 	//print help
@@ -47,99 +47,95 @@ func (cm *Cmd) Run() {
 	}
 }
 
-//parse internal commend
-//like --help, --env, --device
+// parse internal commend
+// like --help, --env, --device
 func (cm *Cmd) parseInternalCmd() {
 
 	arg := string(os.Args[1])
 	cmd := strings.Trim(arg, InternalCmdPrefix)
 
 	switch cmd {
-		case InternalCmdHelp:
-			cm.printHelpMessage()
-			break
-		case InternalCmdEnv:
-			fmt.Println("External plug-in path : "+cm.plugHandle.dir)
-			break
-		case InternalCmdList:
-			cm.plugHandle.PrintList()
-			break
-		case InternalCmdVer:
-			fmt.Println(cxt.Version)
-			break
-		case InternalDevice:
-			cm.printDevice()
-			break
+	case InternalCmdHelp:
+		cm.printHelpMessage()
+		break
+	case InternalCmdEnv:
+		fmt.Println("External plug-in path : " + cm.plugHandle.dir)
+		break
+	case InternalCmdList:
+		cm.plugHandle.PrintList()
+		break
+	case InternalCmdVer:
+		fmt.Println(cxt.Version)
+		break
+	case InternalDevice:
+		cm.printDevice()
+		break
 	}
 	os.Exit(1)
 }
 
-//usage
-func (cm *Cmd) printHelpMessage()  {
+// usage
+func (cm *Cmd) printHelpMessage() {
 
 	fmt.Println("==================================================================================")
 	fmt.Println("[Usage]")
 	fmt.Println("")
-	fmt.Println("    go-sniffer [device] [plug] [plug's params(optional)]")
+	fmt.Println("    mdpi [device] [plug] [plug's params(optional)]")
 	fmt.Println()
 	fmt.Println("    [exp]")
-	fmt.Println("          go-sniffer en0 redis          Capture redis packet")
-	fmt.Println("          go-sniffer en0 mysql -p 3306  Capture mysql packet")
+	fmt.Println("          mdpi en0 redis          Capture redis packet")
+	fmt.Println("          mdpi en0 mysql -p 3306  Capture mysql packet")
 	fmt.Println()
-	fmt.Println("    go-sniffer --[commend]")
-	fmt.Println("               --help \"this page\"")
-	fmt.Println("               --env  \"environment variable\"")
-	fmt.Println("               --list \"Plug-in list\"")
-	fmt.Println("               --ver  \"version\"")
-	fmt.Println("               --dev  \"device\"")
+	fmt.Println("    mdpi --[commend]")
+	fmt.Println("         --help \"this page\"")
+	fmt.Println("         --env  \"environment variable\"")
+	fmt.Println("         --list \"Plug-in list\"")
+	fmt.Println("         --ver  \"version\"")
+	fmt.Println("         --dev  \"device\"")
 	fmt.Println("    [exp]")
-	fmt.Println("          go-sniffer --list \"show all plug-in\"")
+	fmt.Println("         mdpi --list \"show all plug-in\"")
 	fmt.Println()
 	fmt.Println("==================================================================================")
 	cm.printDevice()
 	fmt.Println("==================================================================================")
 }
 
-//print plug-in list
+// print plug-in list
 func (cm *Cmd) printPlugList() {
 	l := len(cm.plugHandle.InternalPlugList)
 	l += len(cm.plugHandle.ExternalPlugList)
-	fmt.Println("#    Number of plug-ins : "+strconv.Itoa(l))
+	fmt.Println("#    Number of plug-ins : " + strconv.Itoa(l))
 }
 
-//print device
+// print device
 func (cm *Cmd) printDevice() {
-	ifaces, err:= net.Interfaces()
+	ifaces, err := net.Interfaces()
 	if err != nil {
 		panic(err)
 	}
 	for _, iface := range ifaces {
 		addrs, _ := iface.Addrs()
-		for _,a:=range addrs {
+		for _, a := range addrs {
 			if ipnet, ok := a.(*net.IPNet); ok {
 				if ip4 := ipnet.IP.To4(); ip4 != nil {
-					fmt.Println("[device] : "+iface.Name+" : "+iface.HardwareAddr.String()+"  "+ip4.String())
+					fmt.Println("[device] : " + iface.Name + " : " + iface.HardwareAddr.String() + "  " + ip4.String())
 				}
 			}
 		}
 	}
 }
 
-//Parameters needed for plug-ins
-func (cm *Cmd) parsePlugCmd()  {
+// Parameters needed for plug-ins
+func (cm *Cmd) parsePlugCmd() {
 
 	if len(os.Args) < 3 {
 		fmt.Println("not found [Plug-in name]")
-		fmt.Println("go-sniffer [device] [plug] [plug's params(optional)]")
+		fmt.Println("mdpi [device] [plug] [plug's params(optional)]")
 		os.Exit(1)
 	}
 
-	cm.Device  = os.Args[1]
-	plugName  := os.Args[2]
-	plugParams:= os.Args[3:]
+	cm.Device = os.Args[1]
+	plugName := os.Args[2]
+	plugParams := os.Args[3:]
 	cm.plugHandle.SetOption(plugName, plugParams)
 }
-
-
-
-
